93c8ad292a8ca7897cad2f12d47109b154cb1279
Pablo de la Peña <p@shedcollective.org>
2016-06-09 11:34:56 +0100

    Added migration - extended size of question label field

---
+++ migrations/2.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Migration:   2
+ * Started:     09/06/2016
+ * Finalised:   09/06/2016
+ *
+ * @package     Nails
+ * @subpackage  module-form-builder
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nailsapp\ModuleFormBuilder;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration2 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query("ALTER TABLE `{{NAILS_DB_PREFIX}}formbuilder_form_field` CHANGE `label` `label` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';");
+    }
+}
