afe0d3f2dece65c7c4f041f9c3ee90082046b17e
Pablo de la Peña <p@shedcollective.org>
2016-06-15 10:25:26 +0100

    Added Likert - Usefulness Field Type

---
+++ src/FormBuilder/FieldType/LikertUsefulness.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This class provides the "Likert - Usefulness" field type
+ *
+ * @package     Nails
+ * @subpackage  module-form-builder
+ * @category    Controller
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\FormBuilder\FormBuilder\FieldType;
+
+use Nails\Factory;
+
+class LikertUsefulness extends Likert
+{
+    const LABEL = 'Likert - Usefulness';
+
+    /**
+     * The terms to use in this likert question
+     * @var array
+     */
+    protected $aLikertTerms = array(
+        'Very Useful',
+        'Useful',
+        'Neither',
+        'Not Useful',
+        'Not at all Useful'
+    );
+}
